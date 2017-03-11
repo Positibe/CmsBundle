fd5d06c66f70ae021292e733536a243e1b3d1dc6
Pedro Carlos Abreu <pcabreus@gmail.com>
2017-03-11 08:05:36 -0500

    Integrating OrmSeoBundle into this bundle

---
+++ Entity/SeoMetadataTranslation.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * This file is part of the PositibeLabs Projects.
+ *
+ * (c) Pedro Carlos Abreu <pcabreus@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Positibe\Bundle\ContentBundle\Entity;
+
+use Gedmo\Translatable\Entity\MappedSuperclass\AbstractTranslation;
+use Doctrine\ORM\Mapping as ORM;
+
+
+/**
+ * Class SeoMetadataTranslation
+ * @package Positibe\Bundle\ContentBundle\Entity
+ *
+ * @ORM\Table(name="positibe_seo_metadata_translations", indexes={
+ *      @ORM\Index(name="positibe_seo_metadata_translation_idx", columns={"locale", "object_class", "field", "foreign_key"})
+ * })
+ * @ORM\Entity(repositoryClass="Gedmo\Translatable\Entity\Repository\TranslationRepository")
+ *
+ * @author Pedro Carlos Abreu <pcabreus@gmail.com>
+ */
+class SeoMetadataTranslation extends AbstractTranslation {
+
+} 
--- PositibeContentBundle.php
+++ PositibeContentBundle.php
@@ -2,6 +2,7 @@
 
 namespace Positibe\Bundle\ContentBundle;
 
+use Doctrine\Bundle\DoctrineBundle\DependencyInjection\Compiler\DoctrineOrmMappingsPass;
 use Positibe\Bundle\ContentBundle\DependencyInjection\Compiler\ContentCompilerPass;
 use Positibe\Bundle\ContentBundle\DependencyInjection\Compiler\ResourceServicesCompilerPass;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
@@ -13,5 +14,15 @@ class PositibeContentBundle extends Bundle
     {
         $container->addCompilerPass(new ContentCompilerPass());
         $container->addCompilerPass(new ResourceServicesCompilerPass());
+        $container->addCompilerPass(
+            DoctrineOrmMappingsPass::createXmlMappingDriver(
+                array(
+                    realpath(__DIR__ . '/Resources/config/doctrine-model') => 'Symfony\Cmf\Bundle\SeoBundle\Model',
+                ),
+                array('cmf_seo.dynamic.persistence.orm.manager_name'),
+                'cmf_seo.backend_type_orm',
+                array('CmfSeoBundle' => 'Symfony\Cmf\Bundle\SeoBundle\Model')
+            )
+        );
     }
 }
